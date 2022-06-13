6e03db8aa4789a2443e3f1a4527bdea8956aff89
arushijain-20 <arushijain0202@gmail.com>
2022-06-13 15:23:00 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
   return (
     <div className="App">
       <h1 className="heading">Gaga Health</h1>
-      <BrowserRouter basename="/prescription">
+      <BrowserRouter>
         <Routes>
           <Route
             path="/"
